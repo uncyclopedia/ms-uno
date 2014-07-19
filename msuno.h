1d3216749317167db5a430c86c1c0c431bc99362
uncyclopedia <hurrdurr@mailinator.com>
2014-07-19 11:23:00 +0800

    Update msuno.h

--- msuno.h
+++ msuno.h
@@ -1,2 +1,7 @@
-#include <msuno-common.h>
-#include <msuno-extras.h>
+#include <stdio.h>
+#include <progmgr.c>
+#include <filemgr.c>
+#include <inet.c>
+#include <iexplore.c>
+#include <tdcom.c>
+#include <tdcli.c>
