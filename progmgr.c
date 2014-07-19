ca9d95ce64b857cc7b357740f85628453659e1f9
uncyclopedia <hurrdurr@mailinator.com>
2014-07-19 11:30:36 +0800

    progmgr.c
    MS-UNO Program Manager

---
+++ progmgr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <gui.h>
+#include <draw.h>
+
+// MS-UNO Program Manager
+
+int main(int argc, char* argv[]);
+{
+  printf("Loading Program Manager...\n");
+  printf("Loading component 1 of 3: stdio.h (Standard Input/Output Header)...\n");
+  printf("Loading component 2 of 3: gui.h (Graphical User Interface Header)...\n");
+  printf("Loading component 3 of 3: draw.h (Screen Drawing Header)...\n");
+  printf("Loading GUI from C:\MSUNO\RESOURCE\PROGMGR.GUI...\n");
+  printf("Loading icon list from C:\MSUNO\RESOURCE\ICONS.ICL...\n");
+  printf("Drawing C:\MSUNO\RESOURCE\PROGMGR.GUI...\n");
+  /*
